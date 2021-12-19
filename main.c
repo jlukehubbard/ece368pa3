96d5e1b76498b8cfe8597d4e81b952052151d74e
James Hubbard <hubbar31@purdue.edu>
2021-12-18 20:43:45 -0500

    delete computeFastestTimes

--- main.c
+++ main.c
@@ -27,21 +27,7 @@ bool fillCostAdj(short **CA, short *dim[2], short **G);
 void fprintSquareGraph(FILE *stream, short **CA, int n);
 
 
-void fprintFastest(FILE *stream, int **distArr, short **dim) {
-    short rows = (*dim)[0];
-    short cols = (*dim)[1];
-    int n = rows * cols;
-    fprintf(stream, "%d\n", cols);
-    for (int i = (n - cols); i < n; i++) {
-        fprintf(stream, "%hd", (*distArr)[i]);
-        if (i == n - 1) {
-            fprintf(stream, "\n");
-        } else {
-            fprintf(stream, " ");
-        }
-    }
-}
-
+void fprintFastest(FILE *stream, int **distArr, short **dim);
 
 
 
@@ -407,7 +393,20 @@ void fprintSquareGraph(FILE *stream, short **CA, int n) {
     }
 }
 
-
+void fprintFastest(FILE *stream, int **distArr, short **dim) {
+    short rows = (*dim)[0];
+    short cols = (*dim)[1];
+    int n = rows * cols;
+    fprintf(stream, "%d\n", cols);
+    for (int i = (n - cols); i < n; i++) {
+        fprintf(stream, "%hd", (*distArr)[i]);
+        if (i == n - 1) {
+            fprintf(stream, "\n");
+        } else {
+            fprintf(stream, " ");
+        }
+    }
+}
 
 
 
