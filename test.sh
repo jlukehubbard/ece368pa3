c5f0145d3bb233725f93921e7bd724edd3ec331c
James Hubbard <hubbar31@purdue.edu>
2021-12-18 20:35:20 -0500

    start from before the bug was introduced

--- test.sh
+++ test.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 
-git add --all && git commit -m "." && gcc -std=c99 -pedantic -Wvla -Wall -Wshadow -g *.c -o pa3 && ./pa3 ./examples/4_5.b
+
+gcc -std=c99 -pedantic -Wvla -Wall -Wshadow -g *.c -o pa3 && ./pa3 ./examples/4_5.b
